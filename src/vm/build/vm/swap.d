8f3420a86622f98a16ac05e7f30ff3bc8cd46009
Hyungrok Kim <q0115643@kaist.ac.kr>
2018-11-16 14:43:16 +0000

    fuck 21/109

--- src/vm/build/vm/swap.d
+++
@@ -1,8 +0,0 @@
-vm/swap.o: ../../vm/swap.c ../../vm/swap.h ../../lib/kernel/list.h \
-  ../../lib/stdbool.h ../../lib/stddef.h ../../lib/stdint.h \
-  ../../vm/page.h ../../lib/kernel/hash.h ../../lib/kernel/list.h \
-  ../../lib/user/syscall.h ../../lib/debug.h ../../filesys/file.h \
-  ../../filesys/off_t.h ../../filesys/off_t.h ../../vm/frame.h \
-  ../../threads/palloc.h ../../threads/thread.h ../../threads/synch.h \
-  ../../threads/synch.h ../../devices/disk.h ../../lib/inttypes.h \
-  ../../threads/vaddr.h ../../threads/loader.h ../../userprog/pagedir.h
